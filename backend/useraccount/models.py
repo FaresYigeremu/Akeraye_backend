cf3b8972ff4d3a3e6a3e1b789c6b7bfb78cfea8e
Fares <119789181+Desray19@users.noreply.github.com>
2025-06-04 23:15:25 +0300

    changed name to username

--- backend/useraccount/models.py
+++ backend/useraccount/models.py
@@ -6,26 +6,26 @@ from django.db import models
 
 
 class CustomUserManager(UserManager):
-    def _create_user(self, name, email, password, **extra_fields):
+    def _create_user(self, username, email, password, **extra_fields):
         if not email:
             raise ValueError("You have not specified a valid e-mail address")
     
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
         return user
 
-    def create_user(self, name=None, email=None, password=None, **extra_fields):
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(name, email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
     
-    def create_superuser(self, name=None, email=None, password=None, **extra_fields):
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self._create_user(name, email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
